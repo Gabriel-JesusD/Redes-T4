a2b583b7b1556138dc75e04a60130432a92994ce
GabrielDeJesus <gabriel.jdantas1@gmail.com>
2022-02-26 04:14:35 -0300

    Casos 1-5
    Tudo funcionando aparentemente, tomou algumas horas, porém nada muito complexo, maior dificuldade relacionada a falta de conhecimento do uso da função replace, a qual não possuía conhecimento de ser uma função com retorno, que por si deve ser armazenado

--- slip.py
+++ slip.py
@@ -1,3 +1,8 @@
+from dataclasses import replace
+from re import S
+import struct
+
+
 class CamadaEnlace:
     ignore_checksum = False
 
@@ -43,15 +48,38 @@ class Enlace:
     def __init__(self, linha_serial):
         self.linha_serial = linha_serial
         self.linha_serial.registrar_recebedor(self.__raw_recv)
+        self.c = 0
+        self.buffer = b''
 
     def registrar_recebedor(self, callback):
         self.callback = callback
+
+    def transform(self, arr):
+        size = len(arr)
+        c = 0xc0
+        d = 0xdb
+        ans = []
+        for i in arr:
+            if i == d:
+                ans.append(b'\xdb\xdd')
+                size += 1
+            elif i == c:
+                ans.append(b'\xdb\xdc')
+                size += 1
+            else:
+                ans.append(i)
+        return (ans,size)
+                
+
 
     def enviar(self, datagrama):
         # TODO: Preencha aqui com o código para enviar o datagrama pela linha
         # serial, fazendo corretamente a delimitação de quadros e o escape de
         # sequências especiais, de acordo com o protocolo CamadaEnlace (RFC 1055).
-        pass
+
+        datagrama = (datagrama.replace(b'\xdb', b'\xdb\xdd')).replace(b'\xc0', b'\xdb\xdc')
+        datagrama = b'\xc0' + datagrama + b'\xc0'
+        self.linha_serial.enviar(datagrama)
 
     def __raw_recv(self, dados):
         # TODO: Preencha aqui com o código para receber dados da linha serial.
@@ -61,4 +89,24 @@ class Enlace:
         # vir quebrado de várias formas diferentes - por exemplo, podem vir
         # apenas pedaços de um quadro, ou um pedaço de quadro seguido de um
         # pedaço de outro, ou vários quadros de uma vez só.
-        pass
+        for i in dados:
+            byte_field = i.to_bytes(1,byteorder='big')
+            if byte_field == b'\xc0':
+                if len(self.buffer) > 0:
+                    c = self.buffer.count(b'\xc0')
+                    datagrama = self.buffer[1:] if c == 1 else self.buffer
+                    if 0 != len(datagrama):
+                        datagrama = (datagrama.replace(b'\xdb\xdc',b'\xc0')).replace(b'\xdb\xdd',b'\xdb')
+                        try:
+                            self.callback(datagrama)
+                        except:
+                            # ignora a exceção, mas mostra na tela
+                            import traceback
+                            traceback.print_exc()
+                        finally:
+                            # faça aqui a limpeza necessária para garantir que não vão sobrar
+                            # pedaços do datagrama em nenhum buffer mantido por você
+                            self.buffer = b''
+                    self.buffer = b''
+            
+            self.buffer += byte_field
